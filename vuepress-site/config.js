8c880cd1c3ed4a6ffda3c9bb0e6849e9a41f0caa
Aakash <aakash.bathla7@gmail.com>
2025-02-27 13:16:37 +0400

    vault backup: 2025-02-27 13:16:37

---
+++ vuepress-site/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "Your Book Title",
+  description: "Your technical book description",
+  themeConfig: {
+    sidebar: [
+      {
+        title: "Introduction",
+        path: "/introduction.md",
+      },
+      // Add more chapters or sections here
+    ],
+  },
+};
