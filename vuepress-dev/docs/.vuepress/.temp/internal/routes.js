17e3cda3aa834344397beae96fa310f26d3c2108
Aakash <aakash.bathla7@gmail.com>
2025-03-03 19:46:00 +0400

    vault backup: 2025-03-03 19:46:00

--- vuepress-dev/docs/.vuepress/.temp/internal/routes.js
+++ vuepress-dev/docs/.vuepress/.temp/internal/routes.js
@@ -4,6 +4,7 @@ export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Home"} }],
   ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+  ["/Async_Await.html", { loader: () => import(/* webpackChunkName: "Async_Await.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/Async_Await.html.js"), meta: {"title":""} }],
 ]);
 
 if (import.meta.webpackHot) {
---
+++ vuepress-dev/docs/.vuepress/.temp/pages/Async_Await.html.js
@@ -0,0 +1,16 @@
+import comp from "/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/Async_Await.html.vue"
+const data = JSON.parse("{\"path\":\"/Async_Await.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"Async_Await.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
