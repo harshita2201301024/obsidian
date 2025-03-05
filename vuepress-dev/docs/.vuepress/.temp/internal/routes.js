2562077bccae6a6d922ad0f67e2a2d266ed83544
Aakash <aakash.bathla7@gmail.com>
2025-03-05 11:50:19 +0400

    vault backup: 2025-03-05 11:50:19

--- vuepress-dev/docs/.vuepress/.temp/internal/routes.js
+++ vuepress-dev/docs/.vuepress/.temp/internal/routes.js
@@ -1,12 +1,12 @@
 export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Home"} }],
-  ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
-  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
   ["/Async_Await.html", { loader: () => import(/* webpackChunkName: "Async_Await.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/Async_Await.html.js"), meta: {"title":""} }],
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Home"} }],
   ["/for_in.html", { loader: () => import(/* webpackChunkName: "for_in.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/for_in.html.js"), meta: {"title":""} }],
+  ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
   ["/three.html", { loader: () => import(/* webpackChunkName: "three.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/three.html.js"), meta: {"title":""} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/aakashbathla/Desktop/Obsidia/vuepress-dev/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
 
 if (import.meta.webpackHot) {
