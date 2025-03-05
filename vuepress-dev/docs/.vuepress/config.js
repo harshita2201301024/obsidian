d634e66e531a419efac9cab70b775311354d1b2a
Aakash <aakash.bathla7@gmail.com>
2025-03-05 12:09:20 +0400

    vault backup: 2025-03-05 12:09:20

--- vuepress-dev/docs/.vuepress/config.js
+++ vuepress-dev/docs/.vuepress/config.js
@@ -1,6 +1,6 @@
-import { defaultTheme } from "@vuepress/theme-default";
 import { defineUserConfig } from "vuepress/cli";
 import { viteBundler } from "@vuepress/bundler-vite";
+import theme from "./theme";
 
 export default defineUserConfig({
   lang: "en-US",
@@ -9,11 +9,7 @@ export default defineUserConfig({
   description:
     "Master Math, Science, Economics, Accounts & Frontend Development",
 
-  theme: defaultTheme({
-    logo: "https://vuejs.press/images/hero.png",
-
-    navbar: ["/", "/get-started"],
-  }),
+  theme,
 
   bundler: viteBundler(),
 });
---
+++ vuepress-dev/docs/.vuepress/theme.ts
@@ -0,0 +1,27 @@
+import { defaultTheme } from "@vuepress/theme-default";
+
+export default defaultTheme({
+  navbar: [
+    { text: "Home", link: "/" },
+    { text: "Subjects", link: "/subjects/" },
+    { text: "Frontend Courses", link: "/frontend/" },
+    { text: "About Us", link: "/about/" },
+    { text: "Contact", link: "/contact/" },
+  ],
+  sidebar: {
+    "/subjects/": [
+      { text: "Mathematics", link: "/subjects/math/" },
+      { text: "Science", link: "/subjects/science/" },
+      { text: "Economics", link: "/subjects/economics/" },
+      { text: "Accounts", link: "/subjects/accounts/" },
+    ],
+    "/frontend/": [
+      { text: "HTML & CSS", link: "/frontend/html-css/" },
+      { text: "JavaScript", link: "/frontend/javascript/" },
+      { text: "Vue.js", link: "/frontend/vue/" },
+      { text: "React.js", link: "/frontend/react/" },
+    ],
+  },
+  contributors: false,
+  editLink: false,
+});
