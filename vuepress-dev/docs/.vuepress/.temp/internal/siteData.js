bc38f73fe516fc0bf31b938bc9e4d87a35f24dd2
Aakash <aakash.bathla7@gmail.com>
2025-03-05 11:58:19 +0400

    vault backup: 2025-03-05 11:58:19

--- vuepress-dev/docs/.vuepress/.temp/internal/siteData.js
+++ vuepress-dev/docs/.vuepress/.temp/internal/siteData.js
@@ -1,4 +1,4 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"Bathla Tuition Center\",\"description\":\"My first VuePress Site\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"Welcome to Bathla Tuition Center!\",\"description\":\"At Bathla Tuition Center, we provide expert guidance in Mathematics, Science, Economics, and Accounts to help students excel in their academics. Our goal is to simplify complex concepts and make learning engaging and effective.<br/>In addition to offline tuition, we are expanding into online frontend development courses, where students can learn HTML, CSS, JavaScript, Vue.js, and React.js from scratch. Whether you’re a beginner or looking to enhance your skills, our structured courses will provide hands-on experience.<br/>This website will serve as a central hub for all our course materials and documentation—covering everything from academic subjects to frontend development tutorials. Stay tuned for updates on new courses, study resources, and more!\",\"head\":[],\"locales\":{}}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
--- vuepress-dev/docs/.vuepress/config.js
+++ vuepress-dev/docs/.vuepress/config.js
@@ -5,8 +5,9 @@ import { viteBundler } from "@vuepress/bundler-vite";
 export default defineUserConfig({
   lang: "en-US",
 
-  title: "Bathla Tuition Center",
-  description: "My first VuePress Site",
+  title: "Welcome to Bathla Tuition Center!",
+  description:
+    "At Bathla Tuition Center, we provide expert guidance in Mathematics, Science, Economics, and Accounts to help students excel in their academics. Our goal is to simplify complex concepts and make learning engaging and effective.<br/>In addition to offline tuition, we are expanding into online frontend development courses, where students can learn HTML, CSS, JavaScript, Vue.js, and React.js from scratch. Whether you’re a beginner or looking to enhance your skills, our structured courses will provide hands-on experience.<br/>This website will serve as a central hub for all our course materials and documentation—covering everything from academic subjects to frontend development tutorials. Stay tuned for updates on new courses, study resources, and more!",
 
   theme: defaultTheme({
     logo: "https://vuejs.press/images/hero.png",
